ec98faec8cc800836389fd756040569036d93066
JuanRomo-dev <juanromo@ucm.es>
2024-04-26 00:18:36 +0200

    aa

---
+++ tfg/src/pages/api/mongodb/connection.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+const MONGO_URI = process.env.MONGODB_URI as string;
+const DATABASE_NAME = process.env.DATABASE_NAME as string;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log(MONGO_URI)
+    const client = new MongoClient(MONGO_URI);
+
+    if (req.method === 'GET') {
+        try {
+            await client.connect();
+            const db = client.db(DATABASE_NAME);
+            console.log("Connection with MongoDB established!");
+            res.status(200);
+        } catch (e) {
+            console.error('Error while connecting to MongoDB', e);
+            throw e;
+        }
+    }
+};
+  
