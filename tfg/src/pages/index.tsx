d16722c8b63b42b68557947c011ede6d2a45d316
Juan Romo <72462333+JuanRomo-dev@users.noreply.github.com>
2024-05-18 16:04:41 +0200

    Vista formulario

--- tfg/src/pages/index.tsx
+++ tfg/src/pages/index.tsx
@@ -37,38 +37,57 @@ const IndexPage = () => {
   };
 
   return (
-    <div className="text-black">
-      <h1>Por favor, selecciona tu tipo de usuario.</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input
-            type="radio"
-            value="medico"
-            checked={userType === 'medico'}
-            onChange={handleUserTypeChange}
-          />
-          Médico
-        </label>
-        <label>
-          <input
-            className="ml-4 mr-4 border-black border-2 rounded-md"
-            type="radio"
-            value="paciente"
-            checked={userType === 'paciente'}
-            onChange={handleUserTypeChange}
-          />
-          Paciente
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-          ></input>
-        </label>
-        <button type="submit">Confirmar</button>
-      </form>
+    <div className="flex flex-col items-center h-screen justify-center  bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Por favor, selecciona tu tipo de usuario.
+      </h1>
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md text-center p-4 items-center">
+        <form
+          className="flex flex-col justify-items-center items-center space-y-5"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-row gap-x-5">
+            <label className="block text-lg font-medium">
+              Tipo de usuario:
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                className="form-radio h-4 w-4"
+                type="radio"
+                value="medico"
+                checked={userType === 'medico'}
+                onChange={handleUserTypeChange}
+              />
+              <span>Médico</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                className="form-radio h-4 w-4"
+                type="radio"
+                value="paciente"
+                checked={userType === 'paciente'}
+                onChange={handleUserTypeChange}
+              />
+              <span>Paciente</span>
+            </label>
+          </div>
+          <label className="block text-lg font-medium">
+            Email:
+            <input
+              className="ml-2 border-2 border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+            ></input>
+          </label>
+          <button
+            type="submit"
+            className="w-1/2 py-2 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-900 transition duration-300"
+          >
+            Confirmar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
